5d814c3eb521c3ce10c2d66f4383aba65a6d633c
Anshuman Kashyap <mr.anshumankashyap@gmail.com>
2021-09-23 11:50:29 +0530

    Import Export Part 1

---
+++ 04.09.2021/Components/navbar.js
@@ -0,0 +1,14 @@
+function navbar(){
+    return `
+    <ul>
+  <li><a class="active" href="index.html">Home</a></li>
+  <li><a href="ROD.html">Recipe Of Day</a></li>
+  <li><a href="TR.html">Trending Recipe</a></li>
+</ul>
+    `;
+}
+
+
+
+
+export default navbar;
